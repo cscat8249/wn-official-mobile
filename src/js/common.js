5f0803c7fbff82afc9be89053033389cbb70156c
parksoomin <soomin8248@gmail.com>
2022-03-17 09:21:45 +0900

    세계음식 추가

--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     let hamburger = document.querySelector('.hamburger');
     let mobile_nav = document.querySelector('.mobile_nav');
-    let close_wrap = document.querySelector('.close_wrap');
+    // let close_wrap = document.querySelector('.close_wrap');
     let btn_state = 0;
 
     hamburger.addEventListener('click',function(){
@@ -14,15 +14,15 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    close_wrap.addEventListener('click',function(){
-        if(btn_state == 0) {
-            mobile_nav.classList.add('on');
-            btn_state = 1;
-        } else {
-            mobile_nav.classList.remove('on');
-            btn_state = 0;
-        }
-    });
+    // close_wrap.addEventListener('click',function(){
+    //     if(btn_state == 0) {
+    //         mobile_nav.classList.add('on');
+    //         btn_state = 1;
+    //     } else {
+    //         mobile_nav.classList.remove('on');
+    //         btn_state = 0;
+    //     }
+    // });
 
     // nav 이동 이벤트
     document.querySelectorAll('.mobile_nav_wrap ul li a').forEach(li => {
