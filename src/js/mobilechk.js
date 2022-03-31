c5c8a7e2040397341e854692b4f1f83d830758ec
parksoomin <soomin8248@gmail.com>
2022-03-31 17:10:37 +0900

    아이콘 추가

--- src/js/mobilechk.js
+++ src/js/mobilechk.js
@@ -1,36 +1,44 @@
+function getCookie(name) {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+        if (cookies[i].indexOf("=") == -1) {
+            if (name == cookies[i])
+                return "";
+        } else {
+            var crumb = cookies[i].split("=");
+            if (name == crumb[0].trim())
+                return unescape(crumb[1].trim());
+        }
+    }
+};
+var desktopModeTF = getCookie("DesktopMode");
+var Scale = getCookie("DesktopModeScale");
+var defWidth = 1170;
+if (desktopModeTF == "true") {
+    document
+            .write('<meta name="viewport" content="width='+defWidth+', user-scalable=yes, initial-scale='+Scale+'">');
+} else {
+    document
+            .write('<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">');
+}
+function desktopMode() {
+    if(getCookie("DesktopMode") == "true"){
+        setModeCookie(false);
+    }else{
+        // alert("다시한번 클릭하시면 원래대로 돌아옵니다.");
+        setModeCookie(true);
+        window.scrollTo(0, 0);
+    }
+    location.reload();
 
-        /* [브라우저 ready 상태 정의 : 최초 호출 상태] */
-        document.addEventListener("DOMContentLoaded", ready);
-        function ready(){
-            // console.log("");            
-            // console.log("[window ready] : [start]");
-            // console.log("");
-
-
-            // [현재 접속된 url 정보 및 접속 브라우저 확인]
-            // [카카오톡 인앱 브라우저 >> 안드로이드 모바일 기종 인 경우 >> 크롬 브라우저 이동 실시]
-            var Url = location.href;
-            var Agent = navigator.userAgent.toLowerCase();
-            // console.log("");            
-            // console.log("[window ready] : [접속 Url] : " + Url);
-            // console.log("[window ready] : [접속 Agent] : " + Agent);
-            // console.log("");
-            //alert(Agent);
-            if(Agent.includes("kakao")){ // 카카오 브라우저로 실행 시킨 경우
-                // 먼저, 카카오 인앱 브라우저 닫기
-                location.href = 'kakaotalk://inappbrowser/close';
-                if(navigator.userAgent.match(/iPhone|iPad/i)){ // 아이폰 접속 경우
-                    // console.log("");
-                    // console.log("[window ready] : [접속 모바일] : " + "[아이폰]");
-                    // console.log("");
-                }
-                else { // 안드로이드 접속 경우
-                    // console.log("");
-                    // console.log("[window ready] : [접속 모바일] : " + "[안드로이드]");
-                    // console.log("");
-                    // 크롬으로 새창 열기
-                    location.href='intent://'+location.href.replace(/https?:\/\//i,'')+'#Intent;scheme=http;package=com.android.chrome;end';
-                }
-            }
-
-        };
+}
+function setModeCookie(switchOn){
+    var now = new Date();
+    var time = now.getTime();
+    time += 3600 * 1000;
+    now.setTime(time);
+    document.cookie ='DesktopMode='+switchOn +'; expires=' + now.toUTCString() ;
+    if(switchOn){
+        document.cookie = "DesktopModeScale=" + $('html').width() / defWidth +'; expires=' + now.toUTCString() ;;
+    }
+}
