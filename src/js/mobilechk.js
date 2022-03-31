37e54200d2572a457bba1860d862f59a5bac9221
parksoomin <soomin8248@gmail.com>
2022-03-31 18:51:04 +0900

    PC웹 이동

--- src/js/mobilechk.js
+++ src/js/mobilechk.js
@@ -1,44 +1,7 @@
-function getCookie(name) {
-    var cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++) {
-        if (cookies[i].indexOf("=") == -1) {
-            if (name == cookies[i])
-                return "";
-        } else {
-            var crumb = cookies[i].split("=");
-            if (name == crumb[0].trim())
-                return unescape(crumb[1].trim());
-        }
-    }
-};
-var desktopModeTF = getCookie("DesktopMode");
-var Scale = getCookie("DesktopModeScale");
-var defWidth = 1170;
-if (desktopModeTF == "true") {
-    document
-            .write('<meta name="viewport" content="width='+defWidth+', user-scalable=yes, initial-scale='+Scale+'">');
-} else {
-    document
-            .write('<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">');
-}
-function desktopMode() {
-    if(getCookie("DesktopMode") == "true"){
-        setModeCookie(false);
-    }else{
-        // alert("다시한번 클릭하시면 원래대로 돌아옵니다.");
-        setModeCookie(true);
-        window.scrollTo(0, 0);
-    }
-    location.reload();
-
-}
-function setModeCookie(switchOn){
-    var now = new Date();
-    var time = now.getTime();
-    time += 3600 * 1000;
-    now.setTime(time);
-    document.cookie ='DesktopMode='+switchOn +'; expires=' + now.toUTCString() ;
-    if(switchOn){
-        document.cookie = "DesktopModeScale=" + $('html').width() / defWidth +'; expires=' + now.toUTCString() ;;
-    }
+var mobileKeyWords = new Array('iPhone', 'iPod', 'BlackBerry', 'Android', 'Windows CE', 'LG', 'MOT', 'SAMSUNG', 'SonyEricsson');
+for (var word in mobileKeyWords){
+if (navigator.userAgent.match(mobileKeyWords[word]) == null){
+parent.window.location.href='https://cscat8249.github.io/wn-official/';
+break;
+ }
 }
