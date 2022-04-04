72919c5b33671931fad23a07f0d0e1743f402049
parksoomin <soomin8248@gmail.com>
2022-04-04 10:18:59 +0900

    모바일 체크 해제

--- src/js/mobilechk.js
+++ src/js/mobilechk.js
@@ -1,7 +1,15 @@
-var mobileKeyWords = new Array('iPhone', 'iPod', 'BlackBerry', 'Android', 'Windows CE', 'LG', 'MOT', 'SAMSUNG', 'SonyEricsson');
-for (var word in mobileKeyWords){
-if (navigator.userAgent.match(mobileKeyWords[word]) == null){
-parent.window.location.href='https://cscat8249.github.io/wn-official/';
-break;
+function getCookie( name ) {
+    var nameOfCookie = name + '=';
+    var x = 0;
+    while ( x <= document.cookie.length ) {
+       var y = (x + nameOfCookie.length);
+       if ( document.cookie.substring( x, y ) == nameOfCookie ) {
+          if ( (endOfCookie=document.cookie.indexOf( ';', y )) == -1 )
+             endOfCookie = document.cookie.length;
+             return unescape( document.cookie.substring( y, endOfCookie ) );
+       }
+       x = document.cookie.indexOf( ' ', x ) + 1;
+       if ( x == 0 ) break;
+    }
+    return '';
  }
-}
