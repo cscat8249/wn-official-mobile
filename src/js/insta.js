ec80026784154a4091911da9df586e5f7f8a3cfa
parksoomin <soomin8248@gmail.com>
2022-05-06 09:12:23 +0900

    인스타 코드 수정

--- src/js/insta.js
+++ src/js/insta.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
      //인스타 피드 이벤트
      const feed = new Instafeed({
-        accessToken: 'IGQVJYMFhtWjNUN2tHRTVwaEhHVldQWmpLSTJ6ZA0ZAWb3VQck5JYnZACdmdpSlZATRlI1WmZArUmE0SHRJemVUOVhTdWE5anpsNDRCTHZAyTE9RVkJGUmdRYVc5aGs1b0YtWWtPSUpyRDJFSjVUOFA4a05ydQZDZD',
+        accessToken: 'IGQVJYT3hVZAmxrc25GR2tkMGgxQUkxQ3pjd3B5OE5lalRVYTl0bmVTUW9melFGblNQdllzLTYzYWx5Yl9sYk9IYkxNZAFgtN1hXMzV0UXB5TW9rOThmTmpzeUt4bU11eExIckxnVnEzOWEwT2VqR1h6SwZDZD',
         target:'gallery',
         template: 
             '<li class="item">'+
